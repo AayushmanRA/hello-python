ffbd0373cfd8ba94d44226428353c0623259749d
Aayushman Rai <aayushmanra@gmail.com>
2024-09-01 08:51:07 +0530

    My first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""name = input("What is your name : ")
+print ("Hello, " + name)"""
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+a = 34
+b = 45
+c = a + b
+
+print(c)
